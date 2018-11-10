7ece6fe757a3685418826a162dfb34d8a1644fa9
Diwaker Singh <diwaker.singh@inmobi.com>
2018-11-11 02:59:51 +0530

    fixed bug with height of image not being taken into account

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
-var AVAILABLE_COLORS=["red","green","blue","yellow","cyan","magenta","purple","black"];
-function newFunction(color,width,height) {
-    var c=document.querySelector("#myCanvas");
-    var ctx=c.getContext("2d");
+var AVAILABLE_COLORS = ["red", "green", "blue", "yellow", "cyan", "magenta", "purple", "black"];
+function newFunction(color, width, height) {
+    var c = document.querySelector("#myCanvas");
+    var ctx = c.getContext("2d");
     ctx.beginPath();
-    ctx.rect(20, 20, width, width);
+    ctx.rect(20, 20, width, height);
     ctx.fillStyle = color;
     ctx.fill();
 }
-function download(){
-    var width=document.querySelector(".width").value||"500";
-    var height=document.querySelector(".height").value||"300";
-    var color=document.querySelector(".color").value||"red";
-    if(AVAILABLE_COLORS.indexOf(color.toLowerCase())===-1){
-        color="red";
+function download() {
+    var width = document.querySelector(".width").value || "500";
+    var height = document.querySelector(".height").value || "300";
+    var color = document.querySelector(".color").value || "red";
+    if (AVAILABLE_COLORS.indexOf(color.toLowerCase()) === -1) {
+        color = "red";
     }
-    console.log({width,color,height})
-    newFunction(color,width,height);
+    console.log({ width, color, height })
+    newFunction(color, width, height);
     var downloadLink = document.createElement("a");
     var image = document.getElementById("myCanvas")
-    .toDataURL("image/png")
-    .replace("image/png", "image/octet-stream");
-    downloadLink.download=color+width+"X"+height;
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+    downloadLink.download = color + width + "X" + height;
     downloadLink.setAttribute("href", image);
     downloadLink.click();
 }
