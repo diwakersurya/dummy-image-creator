9eb53fe3bb402e5f3f12133258555b8721219188
diwakersurya <diwaker.surya@gmail.com>
2020-08-26 00:03:16 +0530

    worked on ui

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
 
 var AVAILABLE_COLORS = ["red", "green", "blue", "yellow", "cyan", "magenta", "purple", "black"];
-function newFunction(color, width, height) {
+
+function getDimensions(){
+    if(validateInputs()){
+        var colorSelector=document.querySelector("#color");
+        var widthInput=document.querySelector(".width");
+        var heightInput=document.querySelector(".height");
+        try{
+            var width = parseInt(widthInput.value,10);
+            var height = parseInt(heightInput.value,10);
+            var color = colorSelector.value;
+            return {width:width,height:height,color:color};
+        }
+        catch(ex){
+            return null;
+        }
+    }
+    return null;
+
+}
+function validateInputs(){
+    var colorSelector=document.querySelector("#color");
+    var widthInput=document.querySelector(".width");
+    var heightInput=document.querySelector(".height");
+    if(!colorSelector.value||!widthInput.value||!heightInput.value){
+        return false;
+    }
+    return true;
+}
+function updatePreview(){
+        //update canvas here
+        const dimensions=getDimensions();
+        if(dimensions){
+            const { width, color, height}=dimensions;
+            updateCanvas(color, width, height)
+        }
+}
+function addColorOptions(){
+    var colorSelector=document.querySelector("#color");
+    AVAILABLE_COLORS.map(color=>{
+        var newOption = new Option(color,color);
+        colorSelector.add(newOption);
+    })
+}
+function addChangeHandlers(){
+    //setup change handler for inputs and select
+    var colorSelector=document.querySelector("#color");
+    var widthInput=document.querySelector(".width");
+    var heightInput=document.querySelector(".height");
+    [colorSelector,widthInput,heightInput].map((input)=>{
+        input.addEventListener("change",updatePreview)
+    })
+}
+//add colors
+(function setup(){
+    addColorOptions();
+    addChangeHandlers();
+    updatePreview();
+})()
+
+function updateCanvas(color, width, height) {
     var c = document.querySelector("#myCanvas");
     var ctx = c.getContext("2d");
     ctx.canvas.width = width;
@@ -11,16 +70,11 @@ function newFunction(color, width, height) {
     ctx.fill();
 }
 function download() {
-    var width = document.querySelector(".width").value || "500";
-    width = parseInt(width, 10);
-    var height = document.querySelector(".height").value || "300";
-    height = parseInt(height, 10);
-    var color = document.querySelector(".color").value || "red";
-    if (AVAILABLE_COLORS.indexOf(color.toLowerCase()) === -1) {
-        color = "red";
-    }
+    const dimensions=getDimensions();
+    if(dimensions){
+    const { width, color, height}=dimensions;
     console.log({ width, color, height })
-    newFunction(color, width, height);
+    updateCanvas(color, width, height);
     var downloadLink = document.createElement("a");
     var image = document.getElementById("myCanvas")
         .toDataURL("image/png")
@@ -28,4 +82,5 @@ function download() {
     downloadLink.download = color + width + "X" + height + ".png";
     downloadLink.setAttribute("href", image);
     downloadLink.click();
+    }
 }
